b3905a7625799e5cd591b5b29a2ac9aa11840a72
Philippe Kirsch <philippe.kirsch@hotmail.fr>
2017-08-26 20:25:57 +0200

    [DE LA MERDE]

--- asm_src/asm.c
+++ asm_src/asm.c
@@ -5,6 +5,7 @@ int main(int ac, char **av)
 	t_asm *e;
 	
 	e = ft_init_asm();
+	ft_printf("maman je suis en vie!!!!!\n\n\n\n\nn\n");
 	if (ac != 2)
 		return (ft_error_asm(e, "not the valid number of arguments. Expected one."));
 	if (ft_file_to_lst_asm(e, av[1]) < 0)
