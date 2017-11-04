2bfe545f76158ce4a23317794feecd7258123269
Philippe Kirsch <philippe.kirsch@hotmail.fr>
2017-11-04 15:00:43 +0100

    [EDIT] activated -a

--- vm/src/op/aff.c
+++ vm/src/op/aff.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   aff.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mtrazzi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: pkirsch <pkirsch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/10 14:49:21 by mtrazzi           #+#    #+#             */
-/*   Updated: 2017/09/10 14:49:22 by mtrazzi          ###   ########.fr       */
+/*   Updated: 2017/11/04 14:59:48 by pkirsch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int	aff(t_env *e, t_prc *prc)
 	unsigned char c;
 
 	c = (unsigned char)get_value(e, REG_CODE, prc, prc->pc + 2);
-	//printf("%c", c); //to change when option aff will be ready
+	if (e->par.opts & OPT_A)
+		printf("Aff: %c\n", c); //to change when option aff will be ready
 	return (0);
 }
