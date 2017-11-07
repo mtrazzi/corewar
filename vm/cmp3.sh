8e26bca8c1decc1548cd8c99a513380ce2a1f999
Michaël Trazzi <mtrazzi@student.42.fr>
2017-11-07 14:20:08 +0100

    cleaned scripts and executables

--- vm/cmp3.sh
+++
@@ -1,9 +0,0 @@
-# CHAMP="../resources/champions_staff/$1.s"
-# ../resources/asm $CHAMP
-echo "executing zaz corewar > zaz3.log"
-../resources/corewar -v 31 ../resources/champions_staff/toto.cor ../resources/champions_staff/champ/helltrain.cor ../resources/champions_staff/champ/helltrain.cor > zaz3.log
-echo "executing my corewar > my3.log"
-./corewar -v 31 ../resources/champions_staff/toto.cor ../resources/champions_staff/champ/helltrain.cor ../resources/champions_staff/champ/helltrain.cor  > my3.log
-echo "doing the diff"
-# diff -y --speed-large-files my3.log zaz3.log
-diff -s --speed-large-files my3.log zaz3.log
--- vm/test1.sh
+++
@@ -1,8 +0,0 @@
-echo "toto mange des bananes"
-echo "philou mange de la viennoise"
-
-
-while [[ true ]]
-do
-	echo "fifi"
-done
