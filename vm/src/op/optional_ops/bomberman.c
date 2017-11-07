278df0281d55f3102638b6edcd6982f8ab4c6a29
Philippe Kirsch <philippe.kirsch@hotmail.fr>
2017-11-07 14:25:30 +0100

    [EDIT] bomberman edge effects

--- vm/src/op/optional_ops/bomberman.c
+++ vm/src/op/optional_ops/bomberman.c
@@ -6,16 +6,17 @@
 /*   By: Philippe <Philippe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/06 14:05:48 by pkirsch           #+#    #+#             */
-/*   Updated: 2017/11/07 13:42:07 by Philippe         ###   ########.fr       */
+/*   Updated: 2017/11/07 14:23:31 by Philippe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 
 #define BPL BYTES_PER_LINE
-#define RADIUS 20
+#define RADIUS 6
+#define BOMB_MSG ("Bomb has been planted, escaping the site")
 
-void	kill_zone(t_env *e, u_int pos, t_prc *prc)
+static void	kill_zone(t_env *e, u_int pos, t_prc *prc)
 {
 	t_dll	*current;
 	t_dll	*tmp;
@@ -37,9 +38,9 @@ void	kill_zone(t_env *e, u_int pos, t_prc *prc)
 	}
 }
 
-void	reset_color(t_env *e, u_int pos, t_prc *prc)
+static void	reset_color(t_env *e, u_int pos, t_prc *prc)
 {
-	e->map_color[pos].color = 0;//
+	e->map_color[pos].color = 0;
 	e->map_color[pos].is_prc = 0;
 	e->map_color[pos].prc_count = 0;
 	e->map_color[pos].color_live = 0;
@@ -48,45 +49,31 @@ void	reset_color(t_env *e, u_int pos, t_prc *prc)
 	kill_zone(e, pos, prc);
 }
 
-u_int	escape_route(u_int pos)
-{
-	u_int i;
-
-	i = 1;
-	while (i < RADIUS && pos / BPL == (pos + i) / BPL)
-		i++;
-	return (i);
-}
-
-
-int		bomberman(t_env *e, t_prc *prc)
+int			bomberman(t_env *e, t_prc *prc)
 {
 	int	r;
 	int pos;
-	int y;
-	int x;
+	int x_y[2];
 
 	r = RADIUS + 1;
 	pos = prc->pc;
-	y = 0;
+	x_y[1] = 0;
 	while (--r > 0)
 	{
-		x = -r - 1;
-		while (++x < r)
+		x_y[0] = -r - 1;
+		while (++(x_y[0]) < r)
 		{
-			if (mod_map(pos + y) - pos < MEM_SIZE / 2
-					&& (pos + y) / BPL == (pos + y + x) / BPL)
-				reset_color(e, mod_map(pos + y + x), prc);
-			if (pos - mod_map(pos - y) < MEM_SIZE / 2
-					&& (pos - y) / BPL == (pos - y + x) / BPL)
-				reset_color(e, mod_map(pos - y + x), prc);
+			if (pos + x_y[1] + x_y[0] <= MEM_SIZE
+			 		&& (pos + x_y[1]) / BPL == (pos + x_y[1] + x_y[0]) / BPL)
+				reset_color(e, mod_map(pos + x_y[1] + x_y[0]), prc);
+			if (pos - x_y[1] + x_y[0] >= 0
+					&& (pos - x_y[1]) / BPL == (pos - x_y[1] + x_y[0]) / BPL)
+				reset_color(e, mod_map(pos - x_y[1] + x_y[0]), prc);
 		}
-		y += BPL;
+		x_y[1] += BPL;
 	}
 	if (e->par.opts & OPT_V4)
-		ft_printf("P %4d | Bomb has been planted, escaping the site +%d\n",
-					prc->id, RADIUS + 1);//
-	// prc->pc = mod_map(prc->pc + RADIUS + 1);
-	prc->pc = mod_map(escape_route(prc->pc));
+		ft_printf("P %4d | %s +%d\n", BOMB_MSG, prc->id, RADIUS + 1);
+	prc->pc = mod_map(prc->pc + RADIUS + 1);
 	return (1);
 }
